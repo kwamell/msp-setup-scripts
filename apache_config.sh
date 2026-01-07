8e963fbeea36f9629ad72a1ce1aaa79211c72ac3
Kwamell Howard <107659595+kwamell@users.noreply.github.com>
2026-01-07 14:36:54 -0500

    Delete apache_config.sh

--- apache_config.sh
+++
@@ -1,5 +0,0 @@
-sudo tee /etc/cron.d/espocrm > /dev/null <<'EOF'
-* * * * * www-data /usr/bin/php /var/www/html/espo/cron.php > /dev/null 2>&1
-EOF
-sudo systemctl restart cron
-sudo rm -f /var/www/html/espo/public/phpinfo.php
